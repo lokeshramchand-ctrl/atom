dbf411e712ed2ca8ad67182b5ebfbff5b8aa5959
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2026-03-15 23:33:04 +0000

    feat : create main page

---
+++ app/src/components/index.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+export default function IndexMain() {
+    return (
+        <section className="min-h-screen flex items-center justify-center px-6">
+            <div className="max-w-5xl w-full bg-bgCard border border-borderCard rounded-xl p-10">
+                <header className="flex justify-between items-start mb-10">
+                    <div>
+                        <h1 className="text-xl font-bold">Atomity Dashboard Component</h1>
+                    </div>
+                </header>
+            </div>
+        </section>
+
+    )
+
+}
